385d3c5dd9717fec3450574f906f85fbdf5e8972
fadm100 <fadm100@gmail.com>
2024-07-03 08:17:06 -0500

    trips files updated

--- VAN/XML_management.py
+++ VAN/XML_management.py
@@ -3,12 +3,12 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('Battery.out.xml')
 root = tree.getroot()
 root.tag
-root[0][0]
 
-# for child in root:
-#     print(child.tag, child.attrib)
+i = 0
+for child in root: 
+    for vehicle in child.findall('vehicle'):
+        print(id)
+        if id != "Delivery_0" and id != "Delivery_1" and id != "Delivery_2" and id != "Delivery_3" and id != "Delivery_4" and id != "Delivery_5":
+            child.remove(vehicle)
     
-# for timestep in root.findall('timestep'):
-#     vehicle = timestep.find('vehicle')
-#     name = timestep.get('name')
-#     print(name, vehicle)
+tree.write('BatteryOut.xml')
